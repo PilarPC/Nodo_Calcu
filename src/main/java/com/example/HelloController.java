21a71c8b5fcf7e256938b72f5d43f1b51936bb82
PilarPC <0217377@up.edu.mx>
2022-11-24 16:09:39 -0600

    Entrega Final

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -2,6 +2,7 @@ package com.example;
 
 import com.example.paquete.Paquete;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -11,6 +12,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController implements Runnable {
 
@@ -23,6 +26,8 @@ public class HelloController implements Runnable {
     int puerto_Nodo = 11000;
     int numeroNodo = 1;
     int puertosNodos[] = {11000, 11001, 11002, 11003, 11004};
+    List<Integer> listaServidores = new ArrayList<>();
+    List<Integer> listaCalculadoras = new ArrayList<>();
 
     public void initialize() {
         Thread hilo1 = new Thread(this);
@@ -31,8 +36,6 @@ public class HelloController implements Runnable {
 
     @Override
     public void run() {
-
-
         while (true) {
             String numeroNodoImprime = numeroNodo + "";
             try {
@@ -48,7 +51,7 @@ public class HelloController implements Runnable {
                     ObjectInputStream flujoEntradaC = new ObjectInputStream(misocketC.getInputStream());
                     Paquete paqueteRecibido = (Paquete) flujoEntradaC.readObject();
                     Platform.runLater(() -> {
-                        VbHistorial.getChildren().add(new Label("Tipo de direccion: " + paqueteRecibido.getIDdireccion()+ " El Operacopn " + paqueteRecibido.getCodigoOperacion() + "\n"));
+                        VbHistorial.getChildren().add(new Label("Tipo de direccion: " + paqueteRecibido.getIDdireccion()+ " con código de operación: " + paqueteRecibido.getCodigoOperacion() +" puerto emisor "+paqueteRecibido.getPuertoEmisor()+ "\n"));
                     });
                    // System.out.println(paqueteRecibido.getMensaje() + " " + paqueteRecibido.getPuertoEmisor() + " " + paqueteRecibido.getIDdireccion() + " Estoy recibiendo en el puerto " + PUERTO_TEMPORAL);
 
@@ -56,9 +59,39 @@ public class HelloController implements Runnable {
                     if(paqueteRecibido.getIDdireccion() != 'N'){//si la direccion no es de un nodo
                         //se lo mando al resto de nodos
                         mandarNodos(paqueteRecibido);
+                        Platform.runLater(() -> {
+                            VbHistorial.getChildren().add(new Label("Mando a nodos "+ "\n"));
+                        });
                     }
-                    if (paqueteRecibido.getCodigoOperacion() == 'a' | paqueteRecibido.getCodigoOperacion() == 'b' | paqueteRecibido.getCodigoOperacion() == 'c' | paqueteRecibido.getCodigoOperacion() == 'd') {
 
+
+                    if (paqueteRecibido.getCodigoOperacion() == 'z'){
+                        boolean esta=buscarEnLista(listaCalculadoras,paqueteRecibido.puertoEmisor);
+                        if(!esta){
+                            listaCalculadoras.add(paqueteRecibido.puertoEmisor);
+                        }
+                        Platform.runLater(() -> {
+                            VbHistorial.getChildren().add(new Label("Agrego a celula cliente "+paqueteRecibido.puertoEmisor+ "\n"));
+                        });
+                    }
+                   if (paqueteRecibido.getCodigoOperacion() == 'x'){
+                        boolean esta=buscarEnLista(listaServidores,paqueteRecibido.puertoEmisor);
+                        if(!esta){
+                            listaServidores.add(paqueteRecibido.puertoEmisor);
+                        }
+
+                        Platform.runLater(() -> {
+                            VbHistorial.getChildren().add(new Label("Agrego a celula servidor "+paqueteRecibido.puertoEmisor+ "\n"));
+                        });
+
+                    }
+                    //Si mi paquete proviene de mi administrador
+                    if(paqueteRecibido.getCodigoOperacion() == 'h'){
+                        paqueteRecibido.setIDdireccion('N');
+                        mandarCalculadoras(paqueteRecibido);
+                    }
+
+                    if (paqueteRecibido.getCodigoOperacion() == 'a' | paqueteRecibido.getCodigoOperacion() == 'b' | paqueteRecibido.getCodigoOperacion() == 'c' | paqueteRecibido.getCodigoOperacion() == 'd') {
                         //MANDO SERVIDOR
                         paqueteRecibido.setIDdireccion('N');
                         mandarServidores(paqueteRecibido);
@@ -84,6 +117,15 @@ public class HelloController implements Runnable {
 
     }
 
+    private boolean buscarEnLista(List<Integer> listaCalculadoras, int puertoEmisor) {
+        for(int nodg:listaCalculadoras){
+            if(nodg==puertoEmisor)
+                return true;
+        }
+        return false;
+    }
+
+
     void mandarNodos(Paquete paqueteRecibido){
         for (int pNodo : puertosNodos) {//le enviamos el mensaje a los nodos
             if (PUERTO_ACTUAL == pNodo) {
@@ -106,39 +148,45 @@ public class HelloController implements Runnable {
         }
     }
     void mandarServidores(Paquete paqueteRecibido) {
-        int puertoServidor = 13000 + numeroNodo - 1;
-        try {
-            //mandar SERVIDOR
-            Socket flujoSalidaS = new Socket("127.0.0.1", puertoServidor);
-            ObjectOutputStream paqueteSalida = new ObjectOutputStream(flujoSalidaS.getOutputStream());
-            paqueteSalida.writeObject(paqueteRecibido);
-            flujoSalidaS.close();
-            String enviarPS = puertoServidor + "";
-            Platform.runLater(() -> {
-                VbHistorial.getChildren().add(new Label("Se mamdo resultado a : " + puertoServidor + "\n"));
-            });
-            System.out.println("se mando resultado a ");
+        for (int servidores : listaServidores){
+            try {
+                //mandar SERVIDOR
+                Socket flujoSalidaS = new Socket("127.0.0.1", servidores);
+                ObjectOutputStream paqueteSalida = new ObjectOutputStream(flujoSalidaS.getOutputStream());
+                paqueteSalida.writeObject(paqueteRecibido);
+                flujoSalidaS.close();
+                String enviarPS = servidores + "";
+                Platform.runLater(() -> {
+                    VbHistorial.getChildren().add(new Label("Se mamdo resultado al puerto del servidor : " + servidores + "\n"));
+                });
+                System.out.println("se mando resultado a ");
 
-        } catch (IOException e) {
-            //System.out.println(" Servidor no está a la escucha");
+            } catch (IOException e) {
+                //System.out.println(" Servidor no está a la escucha");
+            }
         }
     }
 
     void mandarCalculadoras(Paquete paqueteRecibido) {
-        int puertoCliente = 12000 + numeroNodo - 1;
-        try {
-            Socket flujoSalidaC = new Socket("127.0.0.1", puertoCliente);
-            ObjectOutputStream paqueteSalida = new ObjectOutputStream(flujoSalidaC.getOutputStream());
-            paqueteSalida.writeObject(paqueteRecibido);
-            flujoSalidaC.close();
-            Platform.runLater(() -> {
-                VbHistorial.getChildren().add(new Label("Se mamdo resultado a : " + puertoCliente + "\n"));
-            });
-            //System.out.println("se mando resultado a " + puertoCliente);
-        } catch (IOException e) {
-            //System.out.println(puertoCliente + " Cliente no está a la escucha");
+        for (int calculadoras : listaCalculadoras){
+            try {
+                Socket flujoSalidaC = new Socket("127.0.0.1", calculadoras);
+                ObjectOutputStream paqueteSalida = new ObjectOutputStream(flujoSalidaC.getOutputStream());
+                paqueteSalida.writeObject(paqueteRecibido);
+                flujoSalidaC.close();
+                Platform.runLater(() -> {
+                    VbHistorial.getChildren().add(new Label("Se mamdo resultado al puerto del cliente : " + calculadoras + "\n"));
+                });
+                //System.out.println("se mando resultado a " + puertoCliente);
+            } catch (IOException e) {
+                //System.out.println(puertoCliente + " Cliente no está a la escucha");
+            }
         }
-    }
 
+    }
+    @FXML
+    void borrar(ActionEvent event) {
+        VbHistorial.getChildren().clear();
+    }
 
 }
